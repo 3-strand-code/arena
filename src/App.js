45a6113540861e509f9a758143699a3fb9648e4d
ean <eanplatter@gmail.com>
2016-04-14 22:05:45 -0500

    temp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import { render } from 'react-dom'
+import logo from './helm.png'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Arena</h1>
+      <div className='ui container'>
+        <div className='ui hidden divider' />
+        <div className='ui middle aligned center aligned grid'>
+          <div className='five wide column'>
+            <h2 className='ui blue image header'>
+              <img src={logo} className='image' />
+            </h2>
+            <form className='ui large form'>
+              <div className='ui stacked segment'>
+                <div className='field'>
+                  <div className='ui left icon input'>
+                    <i className='user icon'></i>
+                    <input type='text' name='email' placeholder='E-mail address' />
+                  </div>
+                </div>
+                <div className='field'>
+                  <div className='ui left icon input'>
+                    <i className='lock icon'></i>
+                    <input type='password' name='password' placeholder='Password' />
+                  </div>
+                </div>
+                <div className='ui fluid large blue submit button'>Login</div>
+              </div>
+              <div className='ui error message'></div>
+            </form>
+            <div className='ui message'>
+              New to us? <a href='#'>Sign Up</a>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }

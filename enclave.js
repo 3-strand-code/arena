7bc8139e8f5e2fc6e45bd7fede5bb301ae4c19dd
ean <eanplatter@gmail.com>
2016-04-14 21:39:28 -0500

    first

---
+++ enclave.js
@@ -0,0 +1,6 @@
+exports.entry = "src/App.js"
+exports.index = "src/index.html"
+exports.output = "dist"
+exports.port = 8080
+exports.autoInstall = true
+exports.live = true
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Arena</h1>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'))

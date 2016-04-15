72f0bb3efcd64de4546601e87ee2ad802dd63a59
ean <eanplatter@gmail.com>
2016-04-14 22:05:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[3SC Arena](http://3-strand-code.github.io/arena)
+---------
+
+Real time collaborative challenging grounds.
+
+![Areana Demo](http://g.recordit.co/EJkZF4x1ps.gif)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,130 @@
+import * as Babel from 'babel-standalone'
+import faker from 'faker'
+import _ from 'lodash'
+import uuid from 'node-uuid'
 import React from 'react'
 import { render } from 'react-dom'
 
+import ace from 'brace'
+import 'brace/ext/language_tools'
+import 'brace/mode/javascript'
+import 'brace/theme/tomorrow'
+import AceEditor from 'react-ace'
+
+import logo from './helm.png'
+
+const babelConfig = {
+  presets: [
+    'es2015',
+    'stage-0',
+  ]
+}
+
+const challenge = `hi('there') === 'there'`
+const uid = uuid.v4()
+
 class App extends React.Component {
+  state = {
+    code: "// const hi = () => 'there'",
+    challenge,
+    error: null,
+    passed: false,
+  }
+
+  testChallenge = _.throttle(() => {
+    const { code, challenge } = this.state
+    let error
+    let passed = false
+    try {
+      const IIFE = `(function() {${code};return ${challenge}}())`
+      const transformed = Babel.transform(IIFE, babelConfig).code
+      passed = eval(transformed)
+    } catch (err) {
+      error = err.message
+    }
+    this.setState({ passed, error })
+  }, 500)
+
+  componentWillMount() {
+    this.testChallenge()
+  }
+
+  handleChallengeChange = (newValue) => {
+    this.setState({ challenge: newValue })
+    this.testChallenge()
+  }
+
+  handleChangeCode = (code) => {
+    this.setState({ code })
+    this.testChallenge()
+  }
+
+  renderEditors = () => {
+    const { code, passed } = this.state
+    return (
+      <div className='column'>
+        <AceEditor
+          name='editor'
+          mode='javascript'
+          theme='tomorrow'
+          width='100%'
+          value={code}
+          onChange={newVal => this.handleChangeCode(newVal)}
+          editorProps={{$blockScrolling: true}}
+          tabSize={2}
+          useSoftTabs
+        />
+      </div>
+    )
+  }
+
   render() {
+    const { challenge, passed } = this.state
     return (
       <div>
-        <h1>Arena</h1>
+        <div className='ui hidden divider' />
+        <div className='ui text container' style={{ display: 'flex' }}>
+          <div style={{ flex: '0 0 6em' }}>
+            <h2 className='ui center aligned small header'>
+              <img src={logo} className='ui image' />
+              <br />
+              ARENA
+            </h2>
+          </div>
+          <div style={{ flex: '1' }}>
+            <AceEditor
+              name='challenge-editor'
+              mode='javascript'
+              theme='tomorrow'
+              width='100%'
+              height='60px'
+              value={challenge}
+              onChange={this.handleChallengeChange}
+              editorProps={{$blockScrolling: true}}
+              tabSize={2}
+              useSoftTabs
+            />
+          </div>
+        </div>
+
+        <div className='ui hidden divider' />
+
+        <div className='ui center aligned horizontally padded grid'>
+          <div className='column'>
+            {passed ? (
+              <div className='ui inverted green label'>
+                <i className='thumbs up icon' /> Nice job!
+              </div>
+            ) : (
+              <div className='ui label'>
+                Write code to pass the challenge...
+              </div>
+            )}
+          </div>
+        </div>
+        <div className='ui equal width grid'>
+          {this.renderEditors()}
+        </div>
       </div>
     )
   }
